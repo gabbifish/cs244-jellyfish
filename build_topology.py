4727fd2aba7ec924a67fc38b8202b882cca37fc4
reynis <reynis.vazquez@gmail.com>
2018-04-27 13:11:12 -0700

    change topology to build random regular graph of the switches

--- build_topology.py
+++ build_topology.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import CPULimitedHost
@@ -15,24 +16,37 @@ from time import sleep, time
 
 class JellyFishTop(Topo):
     ''' TODO, build your topology here'''
-    def build(self):
+    def build(self, nswitches=10, nhosts=10, k=4):
+        switches = [self.addSwitch('s' + str(i)) for i in range(nswitches)]
+        hosts = [self.addHost('h' + str(h)) for i in range(nhosts)]
 
-            leftHost = self.addHost( 'h1' )
-            rightHost = self.addHost( 'h2' )
-            leftSwitch = self.addSwitch( 's3' )
-            rightSwitch = self.addSwitch( 's4' )
+        linkedSwitches = set()
+        for s1 in switches:
+            switches.remove(s1)
+            for _ in range(k):
+                s2 = random.choice(switches)
+                if s1 != s2 and (s1, s2) not in linkedSwitches \
+                        and (s2, s1) not in linkedSwitches:
+                    self.addLink(s1, s2)
+                    linkedSwitches.add((s1, s2))
+            switches.add(s1)
 
-            # Add links
-            self.addLink( leftHost, leftSwitch )
-            self.addLink( leftSwitch, rightSwitch )
-            self.addLink( rightSwitch, rightHost )
+        leftHost = self.addHost( 'h1' )
+        rightHost = self.addHost( 'h2' )
+        leftSwitch = self.addSwitch( 's3' )
+        rightSwitch = self.addSwitch( 's4' )
+
+        # Add links
+        self.addLink( leftHost, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch )
+        self.addLink( rightSwitch, rightHost )
 
 
 def experiment(net):
-        net.start()
-        sleep(3)
-        net.pingAll()
-        net.stop()
+    net.start()
+    sleep(3)
+    net.pingAll()
+    net.stop()
 
 def main():
 	topo = JellyFishTop()
